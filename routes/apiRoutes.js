ab31d208a926963149d3777ae407edccb329ef23
The_Costa <bryan.costa@roadrunner.com>
2019-05-26 17:38:50 -0700

    Post request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,5 +6,10 @@ module.exports = app => {
         friend.findAll()
             .then(friend => res.json(friend))
             .catch(e => console.log(e))
-    })
+    }),
+        app.post('/friends', (req, res) => {
+            friend.create(req.body)
+                .then(_ => res.sendStatus(200))
+                .catch(e => console.log(e))
+        })
 }
